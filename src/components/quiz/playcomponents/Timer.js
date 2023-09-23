ccce6d881e3e7f0473dc54ef14db51373c37fca7
aamya2004 <93673911+aamya2004@users.noreply.github.com>
2023-09-24 01:50:05 +0530

    timer updates

--- src/components/quiz/playcomponents/Timer.js
+++ src/components/quiz/playcomponents/Timer.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 
-export default function Timer({ setTimeOut, questionNumber }) {
+export default function Timer({ setTimeOut, questionNumber,setQuestionNumber }) {
     const [timer, setTimer] = useState(60);
 
     useEffect(() => {
-        if (timer === 0) return setTimeOut(true);
+        if (timer === 0){ 
+            setQuestionNumber(questionNumber + 1);
+            return setTimeOut(true)
+        };
         const interval = setInterval(() => {
             setTimer((prev) => prev - 1);
         }, 1000);
